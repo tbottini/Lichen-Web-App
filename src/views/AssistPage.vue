b952c803217db1b66e642690a43b2a1c0a698485
tbottini <thomasbottini@protonmail.com>
2023-11-07 20:10:17 +0100

    add page assist

---
+++ src/views/AssistPage.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<Header />
+		<p>
+			Pour toutes demande d'assistance (bug, incompréhension, suppression
+			de compte etc...), veuillez nous contacter à l'adresse
+			<a href="mailto:contact@reseau-lichen.fr"
+				>contact@reseau-lichen.fr</a
+			>
+		</p>
+	</div>
+</template>
+<script>
+import Header from "../components/Header.vue";
+
+export default {
+	components: { Header }
+};
+</script>
+<style scoped>
+* {
+	font-family: Livvic;
+	font-size: 1.4rem;
+	margin-left: 0.5rem;
+}
+</style>
